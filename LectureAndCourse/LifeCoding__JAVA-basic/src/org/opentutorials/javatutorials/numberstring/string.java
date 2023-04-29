42dfa83a365e1bb6b03800644a2d676b51156783
JIyul Kim <130804887+Jiyul-Kim@users.noreply.github.com>
2023-04-29 21:56:03 +0900

    file name modified

---
+++ LectureAndCourse/LifeCoding__JAVA-basic/src/org/opentutorials/javatutorials/numberstring/string.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class string {
+
+	public static void main(String[] args) {
+		System.out.println("생활코딩님은\n\"참 쉽죠?\"\n라고 말했다.");
+	}
+
+}
