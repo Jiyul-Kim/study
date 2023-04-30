6556d4768e413a0e1ba6813fa9347871613da2b6
JIyul Kim <130804887+Jiyul-Kim@users.noreply.github.com>
2023-04-30 16:38:09 +0900

    learn section 10, 11

---
+++ LectureAndCourse/LifeCoding__JAVA-basic/src/org/opentutorials/javatutorials/constant/ConstantDemo.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.constant;
+
+public class ConstantDemo {
+
+	public static void main(String[] args) {
+		long a = 23424223425L ;
+		byte b = 100;
+		short c = 500;
+		
+	}
+
+}
