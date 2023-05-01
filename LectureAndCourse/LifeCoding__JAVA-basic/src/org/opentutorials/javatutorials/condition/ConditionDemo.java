6dd9779f5d655f84ddac5dc7ea5affeceefcdedd
JIyul Kim <130804887+Jiyul-Kim@users.noreply.github.com>
2023-05-01 09:07:53 +0900

    learn java

---
+++ LectureAndCourse/LifeCoding__JAVA-basic/src/org/opentutorials/javatutorials/condition/ConditionDemo.java
@@ -0,0 +1,13 @@
+package org.opentutorials.javatutorials.condition;
+
+public class ConditionDemo {
+
+	public static void main(String[] args) {
+		if(false) {
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+	}
+
+}
