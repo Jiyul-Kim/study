649da39b64a3cf9b20618a65f7d454bf76cad0e6
JIyul Kim <130804887+Jiyul-Kim@users.noreply.github.com>
2023-05-09 00:13:09 +0900

    JS - callback methods - Arrow

--- LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section14__javascript/_11_CallBackMethods/app.js
+++ LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section14__javascript/_11_CallBackMethods/app.js
@@ -1,16 +1,35 @@
 const texts = ["house", "go", "sleep"];
 const caps = texts.map(function (t) {
-    return t.toUpperCase();
-})
+  return t.toUpperCase();
+});
 texts; // ['house', 'go', 'sleep']
 caps; // ['HOUSE', 'GO', 'SLEEP']
 
-const numbers = [1,2,3,4,5];
+const numbers = [1, 2, 3, 4, 5];
 const doubles = numbers.map(function (num) {
-    return num * 2
-})
+  return num * 2;
+});
 
 // 코딩 연습 47번
-const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+const fullNames = [
+  { first: "Albus", last: "Dumbledore" },
+  { first: "Harry", last: "Potter" },
+  { first: "Hermione", last: "Granger" },
+  { first: "Ron", last: "Weasley" },
+  { first: "Rubeus", last: "Hagrid" },
+  { first: "Minerva", last: "McGonagall" },
+  { first: "Severus", last: "Snape" },
+];
 
-const fullName = fullNames.map()
+const firstNames = fullNames.map(function (fistname) {
+  return fistname.first;
+});
+
+const add = function(x, y) {
+    return x + y;
+}
+
+// 코딩 연습 48
+const greet = (name) => {
+    return `Hey ${name}!`
+} 
