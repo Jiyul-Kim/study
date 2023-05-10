7c2435c514a84ae9a8738abb7b9c801766eda7c1
JIyul Kim <130804887+Jiyul-Kim@users.noreply.github.com>
2023-05-10 15:56:34 +0900

    modified

---
+++ LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section23__dom/Selecting/app.js
@@ -0,0 +1,10 @@
+// const allImages = document.getElementsByTagName('img');
+
+// for(let img of allImages){
+//     console.log(img.src)
+// }
+
+const squareImages = document.getElementsByClassName('square');
+for(let img of squareImages){
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/e/e0/Male_Silkie.png"
+}
