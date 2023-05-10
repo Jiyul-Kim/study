1cef54202d761a5387ec1e9ee21761334b05a83c
JIyul Kim <130804887+Jiyul-Kim@users.noreply.github.com>
2023-05-10 16:48:31 +0900

    [JS 공부] - DOM

--- LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section23__dom/Selecting/app.js
+++ LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section23__dom/Selecting/app.js
@@ -4,7 +4,12 @@
 //     console.log(img.src)
 // }
 
-const squareImages = document.getElementsByClassName('square');
-for(let img of squareImages){
-    img.src = "https://upload.wikimedia.org/wikipedia/commons/e/e0/Male_Silkie.png"
-}
+// const squareImages = document.getElementsByClassName('square');
+// for(let img of squareImages){
+//     img.src = "https://upload.wikimedia.org/wikipedia/commons/e/e0/Male_Silkie.png"
+// }
+
+// 코딩 연습 52
+
+const doneTodos = document.querySelectorAll(".done");
+const checkbox = document.querySelector("input:nth-of-type(2)")
