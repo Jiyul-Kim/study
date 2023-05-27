acca63c4be275da94b47c30e3412f01cbc6ed9c9
JIyul Kim <130804887+Jiyul-Kim@users.noreply.github.com>
2023-05-28 01:44:21 +0900

    📚공부: make to-do-list using React

---
+++ LectureAndCourse/react/react-practice/src/App.js
@@ -0,0 +1,25 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello () {
+  useEffect(() => {
+    console.log('created! :D')
+    return () => console.log("destroyed!")
+  }, [])
+  
+  return <h1>Hello</h1>
+}
+
+function App() {
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing((prev) => !prev)
+  return (
+    <div>
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
