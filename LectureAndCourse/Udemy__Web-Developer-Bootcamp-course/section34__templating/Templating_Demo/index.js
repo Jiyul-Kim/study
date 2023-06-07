d0d95d7981b80b9667256d82a8ba2e11fbfaed61
KimJiYul <kjekje041212@gmail.com>
2023-06-07 13:55:01 +0900

    📚공부: get post 차이

--- LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section34__templating/Templating_Demo/index.js
+++ LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section34__templating/Templating_Demo/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path')
 const redditData = require('./data.json')
 
+app.use(express.static(path.join(__dirname,'public')))
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 
@@ -30,7 +32,7 @@ app.get('/r/:subreddit', (req, res)=> {
 
 app.get('/rand', (req, res) => {
     const num = Math.floor(Math.random() * 10 ) + 1;
-    res.render('random', {num: num})
+    res.render('random', {num})
 })
 
 app.listen(3000, () => {
