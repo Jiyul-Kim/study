6c257f8d6a8572b5c3f1e282141feff723cc7319
KimJiYul <kjekje041212@gmail.com>
2023-06-09 13:26:38 +0900

    📚공부: express를 이용한 CRUD 구현

--- LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section35__RESTful-Route/REST/index.js
+++ LectureAndCourse/Udemy__Web-Developer-Bootcamp-course/section35__RESTful-Route/REST/index.js
@@ -2,13 +2,15 @@ const express = require('express')
 const app = express();
 const path = require('path')
 const { v4: uuid } = require('uuid')
+const methodOverride = require('method-override')
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: "jiyul",
@@ -49,6 +51,28 @@ app.get('/comments/:id', (req, res) => {
     const {id} = req.params;
     const comment = comments.find(c => c.id === id)
     res.render('comments/show', {comment})
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', {comment})
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const {id} = req.params;
+    const newCommentText = req.body.comment;
+    // console.log(req.body.comment)
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments')
+    // res.send('succese')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const {id} = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments')
 })
 
 app.get('/tacos', (req, res) => {
